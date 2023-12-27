c84fff4ebe0e0f373c857507716e65b5e818a5c7
Flash-P <154947295+Flash-P@users.noreply.github.com>
2023-12-27 06:10:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lavatarl/avatar",
+    "https://github.com/Flash-P/flash",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
